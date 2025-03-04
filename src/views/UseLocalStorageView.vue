5fe18ca11bc7b716d546b8430794c2ec0ab1823d
Al Zaplin <zaplin.dev@gmail.com>
2025-03-04 09:56:56 +0300

    add(code & notes) For #18 Working with Local and Session Storages by using VueUse utilities

--- src/views/UseLocalStorageView.vue
+++ src/views/UseLocalStorageView.vue
@@ -1,9 +1,22 @@
-<!-- * 16.0 "useLocalStorage" -->
+<!-- * 16.0 "useLocalStorage": работаем с Local Storage. -->
+<!-- * 16.3 "useSessionStorage": если нам нужно более временное решение только на время конкретной браузерной сессии (само-удаляется при закрытии вкладки). А также не делит между разными закладками одного сайта (искл. - дублирование вкладки), как это делается в Local Storage. -->
 <script setup>
-import { ref } from 'vue'
-import { useLocalStorage } from '@vueuse/core'
+import { useLocalStorage, useSessionStorage } from '@vueuse/core'
+
+// 16.1 Чем-то напоминает создание реактивного рефа, но только первым аргументом мы добавляем название ключа, по которому будут хранить данные в хранилище браузера, а вторым его значение.
+const name = useLocalStorage('name', 'Al')
+const surname = useSessionStorage('surname', '')
 </script>
 
+<!-- 16.2 А теперь проверим реактивность "useLocalStorage" в действии и для этого создадим элемент строку ввода и привяжем его к "name". -->
 <template>
-  <div class="text-3xl"></div>
+  <label class="mb-3 block" for=""
+    >Input your surname:<input
+      v-model="surname"
+      type="text"
+      class="ml-2 px-2 py-1 border-2 rounded-sm"
+  /></label>
+  <label for=""
+    >Input your name:<input v-model="name" type="text" class="ml-2 px-2 py-1 border-2 rounded-sm"
+  /></label>
 </template>
